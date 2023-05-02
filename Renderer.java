143e4d8e57be0802c397d9503be6e8b7b3560a2a
Om shankar <110126383+Shankar-2125@users.noreply.github.com>
2023-05-02 10:39:33 +0530

    Add files via upload

--- Renderer.java
+++ Renderer.java
@@ -7,14 +7,14 @@ import javax.swing.JPanel;
 public class Renderer extends JPanel
 {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@Override
-	protected void paintComponent(Graphics g)
-	{
-		super.paintComponent(g);
+    @Override
+    protected void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
 
-		FlappyBird.flappyBird.repaint(g);
-	}
-	
+        FlappyBird.flappyBird.repaint(g);
+    }
+
 }
